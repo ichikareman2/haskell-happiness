901a9cf546b451f334c1c4f67462e7abbe9c614d
kevin quing <kevin@kevian.kevian.com>
2020-02-18 01:13:04 +0800

    ch7
    * fix compile errors
    * pattern matching

--- ch7/ch7.hs
+++ ch7/ch7.hs
@@ -6,9 +6,9 @@ module Ch7 where
 -- Anonymous functions
 -- ====================
 
-triple :: Integer -> Integer
+-- triple :: Integer -> Integer
 -- triple x = x * 3
-(\x -> x * 3) :: Integer -> Integer
+trip1 = (\x -> x * 3) :: Integer -> Integer
 
 -- ====== TO USE IN GHCI ======
 -- :{
@@ -17,7 +17,7 @@ triple :: Integer -> Integer
 -- :}
 
 -- ====== TO APPLY ANONYMOUSLY ======
-(\x -> x * 3) 5
+tripped5 = (\x -> x * 3) 5
 -- 15
 
 
@@ -36,7 +36,7 @@ triple :: Integer -> Integer
 -- Num a => a -> a -> a
 
 -- 3. Next, we’ll practice writing anonymous lambda syntax. For example, one could rewrite:
-addOne x = x + 1
+-- addOne x = x + 1
 addOne = \x -> x + 1 
 
 -- Try to make it so it can still be loaded as a top-level definition by GHCi.
@@ -55,3 +55,59 @@ addFive = \x -> \y -> (if x > y then y else x) + 5
 -- c) Rewrite the following so that it doesn’t use anonymous lambda syntax:
 -- mflip f = \x -> \y -> f y x
 mFlip f x y = f y x
+
+
+
+
+
+-- ====================
+-- Pattern matching
+-- can be used to unpack and expose content of data
+-- and specify behavior depending on data
+-- ====================
+
+isItTwo :: Integer -> Bool
+isItTwo 2 = True
+isItTwo _ = False
+
+-- call `:set -Wall` to catch non exhaustive
+
+newtype Username = Username String
+newtype AccountNumber = AccountNumber Integer
+
+data User = UnregisteredUser | RegisteredUser Username AccountNumber
+
+printUser :: User -> IO()
+printUser UnregisteredUser =
+  putStrLn "UnregisteredUser"
+printUser (RegisteredUser
+            (Username name)
+            (AccountNumber acctNum)) =
+  putStrLn $ name ++ " " ++ show acctNum
+
+
+
+data WherePenguinsLive = 
+    Galapagos
+  | Antarctica
+  | Australia
+  | SouthAfrica
+  | SouthAmerica deriving(Eq,Show)
+
+data Penguin = Peng WherePenguinsLive deriving(Eq,Show)
+
+isSouthAfrica':: WherePenguinsLive-> Bool
+isSouthAfrica' SouthAfrica = True
+isSouthAfrica' _ = False
+
+gimmeWhereTheyLive :: Penguin -> WherePenguinsLive
+gimmeWhereTheyLive (Peng whereitlives) = whereitlives
+
+humboldt = Peng SouthAmerica
+gentoo = Peng Antarctica
+macaroni = Peng Antarctica
+little = Peng Australia
+galapagos = Peng Galapagos
+
+
+
