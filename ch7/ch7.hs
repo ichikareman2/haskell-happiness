db02a0896fb951ee88840d674fc6fb06c711495b
kevin quing <kevin@kevian.kevian.com>
2020-02-18 00:51:07 +0800

    # Exercise: Grab bag

--- ch7/ch7.hs
+++ ch7/ch7.hs
@@ -22,16 +22,18 @@ triple :: Integer -> Integer
 
 
 -- 1. Which (two or more) of the following are equivalent?
-mTh x y z = x * y * z
-mTh x y = \z -> x * y * z
-mTh x = \y -> \z -> x * y * z
-mTh = \x -> \y -> \z -> x * y * z 
+-- A: all of them
+-- mTh x y z = x * y * z
+-- mTh x y z = x * y * z
+-- mTh x = \y -> \z -> x * y * z
+-- mTh = \x -> \y -> \z -> x * y * z 
 
 -- 2. The type of mTh (above) is Num a => a -> a -> a -> a. Which is the type of mTh 3?
-Integer -> Integer -> Integer
-Num a => a -> a -> a -> a
-Num a => a -> a
-Num a => a -> a -> a
+-- A: last one
+-- Integer -> Integer -> Integer
+-- Num a => a -> a -> a -> a
+-- Num a => a -> a
+-- Num a => a -> a -> a
 
 -- 3. Next, we’ll practice writing anonymous lambda syntax. For example, one could rewrite:
 addOne x = x + 1
@@ -43,9 +45,13 @@ addOne = \x -> x + 1
 addOneIfOdd n = case odd n of 
               True -> f n
               False -> n
-              where f n = n + 1
+              -- where f n = n + 1
+              where f = \n -> n + 1
+
 -- b) Rewrite the following to use anonymous lambda syntax:
-addFive x y = (if x > y then y else x) + 5 
+-- addFive x y = (if x > y then y else x) + 5 
+addFive = \x -> \y -> (if x > y then y else x) + 5
 
 -- c) Rewrite the following so that it doesn’t use anonymous lambda syntax:
-mflip f = \x -> \y -> f y x
+-- mflip f = \x -> \y -> f y x
+mFlip f x y = f y x
